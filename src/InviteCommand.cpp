a9ec940b49cef910e9cdaf1118651439d27882ab
Gab <gab@MacBook-Pro-de-Anon.local>
2023-08-30 18:35:34 +0200

    InviteCommand still in progress, add check if user target exist

--- src/InviteCommand.cpp
+++ src/InviteCommand.cpp
@@ -5,12 +5,29 @@ void InviteCommand::execute(const std::vector<std::string>& args, int socket, Se
     std::cout << "InviteCommand executed" << std::endl; //! debug
     if (args.size() < 2)
     {
-        server.sendReply("ERR_NEEDMOREPARAMS :Need more params", client_fd);
+        server.sendReply(":(nom du serveur) 461 " + server.getClients()[client_fd]->getNickname() + " INVITE :Not enough parameters", client_fd);
         return ;
     }
-    std::string nickname = server.getClients()[client_fd]->getNickname();
-    // std::string channel_name = server.getChannel()[client_fd]
-    //voir comment faire pour choper le nom de channel et le filer args[1]
-
     
+    std::string targetNick = args[0];
+    std::string channelName = args[1];
+
+    //s'assurer que le client visé existe
+    Client* targetClient = nullptr;
+    std::map<int, Client*>::iterator it;
+
+    for (it = server.getClients().begin(); it != server.getClients().end(); ++it)
+    {
+        if (it->second->getNickname() == targetNick)
+        {
+            targetClient = it->second;
+            break;
+        }
+    }
+
+    if (!targetClient) {
+        server.sendReply(":(nom du serveur) 401 " + server.getClients()[client_fd]->getNickname() + " " + targetNick + " :No such nick/channel", client_fd);
+        return;
+    }
+
 }
