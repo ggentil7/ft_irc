a9b91c39874830ca29908ea5c6e79a492f4db323
0xnino <86327707+0xNino@users.noreply.github.com>
2023-09-05 21:23:06 +0200

    formatting

--- src/JoinCommand.cpp
+++ src/JoinCommand.cpp
@@ -19,12 +19,12 @@ void JoinCommand::execute(const std::vector<std::string> &args, int client_fd, S
 {
 	std::cout << "Server: JOIN ";
 	std::vector<std::string> newArgs(args);
-	for (std::vector<std::string>::iterator it = newArgs.begin(); it != newArgs.end(); ++it) {
+	for (std::vector<std::string>::iterator it = newArgs.begin(); it != newArgs.end(); ++it)
+	{
 		std::cout << *it << ' ';
 	}
 	std::cout << std::endl;
 
-  
 	if (args.size() < 1)
 	{
 		std::string errMsg = ":ft_irc 461 JOIN :Not enough parameters"; // ERR_NEEDMOREPARAMS
@@ -33,37 +33,34 @@ void JoinCommand::execute(const std::vector<std::string> &args, int client_fd, S
 	}
 
 	std::string channelName = args[0];
-    Channel* targetChannel = server.getChannelByName(channelName);
-    Client *client = server.getClientByFd(client_fd);
+	Channel *targetChannel = server.getChannelByName(channelName);
+	Client *client = server.getClientByFd(client_fd);
 
-    if (!targetChannel) //si le channel existe pas 
-    {
-        //creer le channel
-        targetChannel = new Channel();
-        server.addChannel(channelName, targetChannel);
+	if (!targetChannel) // si le channel existe pas
+	{
+		// creer le channel
+		targetChannel = new Channel();
+		server.addChannel(channelName, targetChannel);
 
-        targetChannel->addClient(client_fd);
+		targetChannel->addClient(client_fd);
 
-        std::string createMsg = "Channel " + channelName + " created.";
-        server.sendReply(createMsg, client_fd);
+		std::string createMsg = "Channel " + channelName + " created.";
+		server.sendReply(createMsg, client_fd);
+	}
 
-    }
+	else
+	{
+		// Vérifiez si le canal est en mode "invite-only" et si l'utilisateur a été invité
+		if (targetChannel->isModeSet(Channel::INVITE_ONLY) && !targetChannel->isUserInvited(client))
+		{
+			std::string errMsg = ":YourServer 473 " + channelName + " :Cannot join channel (+i) - you must be invited";
+			server.sendReply(errMsg, client_fd);
+			return;
+		}
 
-    else
-    {
-        // Vérifiez si le canal est en mode "invite-only" et si l'utilisateur a été invité
-        if (targetChannel->isModeSet(Channel::INVITE_ONLY) && !targetChannel->isUserInvited(client))
-        {
-            std::string errMsg = ":YourServer 473 " + channelName + " :Cannot join channel (+i) - you must be invited";
-            server.sendReply(errMsg, client_fd);
-            return;
-        }
+		// ajoutez le client au canal
+		targetChannel->addClient(client_fd);
+	}
 
-        //ajoutez le client au canal
-        targetChannel->addClient(client_fd);
-
-    }
-
-    targetChannel->broadcastMessage(client->getNickname() + " has joined " + channelName, server);
-
+	targetChannel->broadcastMessage(client->getNickname() + " has joined " + channelName, server);
 }
