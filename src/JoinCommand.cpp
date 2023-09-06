fea01087610fdf203ec4fabf9e9eb83ef09176cf
0xNino <86327707+0xNino@users.noreply.github.com>
2023-09-06 02:57:16 +0200

    cleanup

--- src/JoinCommand.cpp
+++ src/JoinCommand.cpp
@@ -1,20 +1,6 @@
 #include "../includes/Server.hpp"
 #include "../includes/JoinCommand.hpp"
 
-/*
-	Syntax Validation: Make sure the client has sent a valid JOIN command according to the IRC protocol. The simplest form is JOIN #channelname.
-
-	Authentication: Check if the user is authenticated or if the channel requires a special key (password).
-
-	Channel Existence: Check if the channel already exists. If not, create it and add the client as the first member and probably also as the channel operator.
-
-	Add User to Channel: If the channel exists, add the user to the channel's list of members.
-
-	Send Notifications: Notify all members of the channel that a new user has joined.
-
-	Send Reply: Finally, send a reply back to the client to acknowledge the JOIN.
-*/
-
 void JoinCommand::execute(const std::vector<std::string> &args, int client_fd, Server &server)
 {
 	std::cout << "Server: JOIN ";
@@ -48,13 +34,12 @@ void JoinCommand::execute(const std::vector<std::string> &args, int client_fd, S
 		std::string createMsg = "Channel " + channelName + " created.";
 		server.sendReply(createMsg, client_fd);
 	}
-
 	else
 	{
 		// Vérifiez si le canal est en mode "invite-only" et si l'utilisateur a été invité
 		if (targetChannel->isModeSet(Channel::INVITE_ONLY) && !targetChannel->isUserInvited(client))
 		{
-			std::string errMsg = ":YourServer 473 " + channelName + " :Cannot join channel (+i) - you must be invited";
+			std::string errMsg = ":YourServer 473 " + channelName + " :Cannot join channel (+i) - you must be invited"; // ERR_INVITEONLYCHAN
 			server.sendReply(errMsg, client_fd);
 			return;
 		}
@@ -62,6 +47,5 @@ void JoinCommand::execute(const std::vector<std::string> &args, int client_fd, S
 		// ajoutez le client au canal
 		targetChannel->addClient(client_fd);
 	}
-
 	targetChannel->broadcastMessage(client->getNickname() + " has joined " + channelName, server);
 }
