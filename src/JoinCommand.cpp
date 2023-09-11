9dfad9d85304292e3eef0537a544918c98184f23
0xNino <86327707+0xNino@users.noreply.github.com>
2023-09-11 23:14:51 +0200

    fix join messages

--- src/JoinCommand.cpp
+++ src/JoinCommand.cpp
@@ -10,37 +10,34 @@ void JoinCommand::execute(const std::vector<std::string> &args, int client_fd, S
 		return;
 	}
 
-	std::string channelName;
-	if (args.size() == 1 && args[0] == "")
-		channelName = "ft_irc";
-	else
-		channelName = args[0];
+	std::string	channelName = args[0];
 
 	if (channelName[0] != '#')
 		return;
 
-	Channel *targetChannel = server.getChannelByName(channelName);
-	Client *client = server.getClientByFd(client_fd);
+	Client	*client = server.getClientByFd(client_fd);
+	if (!client)
+		return;
 
-	if (!targetChannel) // si le channel existe pas
+	std::string	joinMsg = ":" + client->getNickname() + "!" + client->getUsername() + "@" + client->getHostname() + " JOIN :" + channelName;
+
+	Channel	*targetChannel = server.getChannelByName(channelName);
+	if (!targetChannel)
 	{
-		// creer le channel
 		targetChannel = new Channel();
 		server.addChannel(channelName, targetChannel);
 
 		targetChannel->addClient(client_fd);
 		targetChannel->addOperator(client_fd);
 
-		client->setMode(Client::OPERATOR, true);
-
-		std::string createMsg = "Channel " + channelName + " created.";
-		server.sendReply(createMsg, client_fd);
+		client->setMode(Client::OPERATOR, true); //! use channel operator instead of client operator
+		server.sendReply(joinMsg, client_fd);
 	}
 	else
 	{
 		if (targetChannel->getUserLimit() > 0 && targetChannel->getMembers().size() >= targetChannel->getUserLimit())
 		{
-			server.sendReply(":ft_irc 471 " + server.getClients()[client_fd]->getNickname() + " " +  channelName + " :Cannot join channel (+l) - channel is full", client_fd);
+			server.sendReply(":ft_irc 471 " + server.getClients()[client_fd]->getNickname() + " " +  channelName + " :Cannot join channel (+l) - channel is full", client_fd); // ERR_CHANNELISFULL
 			return;
 		}
 		if (targetChannel->isModeSet(Channel::INVITE_ONLY) && !targetChannel->isUserInvited(client))
@@ -55,8 +52,14 @@ void JoinCommand::execute(const std::vector<std::string> &args, int client_fd, S
 				server.sendReply(":server 475 " + client->getNickname() + " " + channelName + " :Cannot join channel (+k)", client_fd);
 			return;
 		}
-
 		targetChannel->addClient(client_fd);
+		server.sendReply(joinMsg, client_fd);
 	}
-	targetChannel->broadcastMessage(client->getNickname() + " has joined " + channelName, server);
+	targetChannel->broadcastMessage(joinMsg, server);
+	//! Send topic
+	//! Send list of users
+	/*
+		:server 353 YourNick = #channelname :@YourNick User2 User3
+		:server 366 YourNick #channelname :End of NAMES list
+	*/
 }
