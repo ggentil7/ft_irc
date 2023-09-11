4f6f1c09780c332f72aec006d7781851fd9c2768
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2023-09-11 18:57:38 +0200

    UserCmd exeption done

--- src/Server.cpp
+++ src/Server.cpp
@@ -19,6 +19,7 @@ Server::Server(int port, std::string password) : _port(port), _password(password
 	_commandMap["PASS"] = new PassCommand();
 	_commandMap["NICK"] = new NickCommand();
 	_commandMap["USER"] = new UserCommand();
+	_commandMap["userhost"] = new UserCommand();
 	_commandMap["JOIN"] = new JoinCommand();
 	_commandMap["KICK"] = new KickCommand();
 	_commandMap["INVITE"] = new InviteCommand();
@@ -170,6 +171,7 @@ void Server::connectionServer()
 
 	while (true)
 	{
+		usleep(500);
 		fd_set readfds;
 		bzero(&readfds, sizeof(readfds));
 
